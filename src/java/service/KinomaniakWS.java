8016e5da0f42bde650ab82e75faf7767a49f1055
qbassqbass <jakub.palac@gmail.com>
2015-10-30 15:24:59 +0100

    added Hibernate framework and db mapping + WebService testing

--- src/java/service/KinomaniakWS.java
+++ src/java/service/KinomaniakWS.java
@@ -5,9 +5,17 @@
  */
 package service;
 
+//import entity.Movie;
+import entity.*;
+import java.util.ArrayList;
+import utils.HibernateUtil;
+import java.util.List;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
 
 /**
  *
@@ -15,6 +23,21 @@ import javax.jws.WebParam;
  */
 @WebService(serviceName = "KinomaniakWS")
 public class KinomaniakWS {
+    
+    private List executeHQLQuery(String hql) {
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            Query q = session.createQuery(hql);
+            List resultList = q.list();
+            if(!resultList.isEmpty())
+                return resultList;
+            session.getTransaction().commit();
+        } catch (HibernateException he) {
+            he.printStackTrace();
+        }
+        return null;
+    }
 
     /**
      * This is a sample web service operation
@@ -22,5 +45,34 @@ public class KinomaniakWS {
     @WebMethod(operationName = "hello")
     public String hello(@WebParam(name = "name") String txt) {
         return "Hello " + txt + " !";
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "getMovie")
+    public Movie getMovie(@WebParam(name = "id") final int id) {
+        //TODO write your implementation code here:
+        List result = executeHQLQuery("From Movie m where m.id = " + id);
+        
+        return (Movie)result.get(0);
+//        return null;
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "getCast")
+    public Cast getCast(@WebParam(name = "movieid") int movieid) {
+        //TODO write your implementation code here:
+        List result = executeHQLQuery("From Cast c where c.movie.id = " + movieid);
+        if(result != null){
+            ArrayList<Cast> clist = new ArrayList<Cast>();
+            for(Object o: result){
+                clist.add((Cast)o);
+            }
+            return clist.get(0);
+        }
+        return null;
     }
 }
