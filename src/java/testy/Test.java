5a2bef0a5ac17dfcdb856389c2d17bca58c0b181
ADAM <ADAM@YOGA.student.tu.kielce.pl>
2015-12-15 20:40:55 +0100

    generete code

---
+++ src/java/testy/Test.java
@@ -0,0 +1,24 @@
+package testy;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ADAM
+ */
+public class Test {
+    private int lol = 4;
+
+    public int getLol() {
+        return lol;
+    }
+
+    public void setLol(int lol) {
+        this.lol = lol;
+    }
+    
+}
